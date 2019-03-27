559b00bc53b78c122830da5055a2751f50648453
Francesco De Martino <fdm@amazon.com>
2019-03-27 15:06:54 +0100

    Bump version to 2.3.0
    Signed-off-by: Francesco De Martino <fdm@amazon.com>

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ console_scripts = [
     'nodewatcher = nodewatcher.nodewatcher:main',
     'jobwatcher = jobwatcher.jobwatcher:main',
 ]
-version = "2.2.1"
+version = "2.3.0"
 requires = ['boto3>=1.7.55', 'python-dateutil>=2.6.1', 'retrying>=1.3.3', 'future>=0.17.1']
 
 if sys.version_info[:2] == (2, 6):
