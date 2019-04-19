896bbac1993198f69f09f908074b340aabacdade
Francesco De Martino <fdm@amazon.com>
2019-04-19 11:36:50 +0200

    jobwatcher: do not scale-up if CPU contraints cannot be satisfied
    Currently with Slurm and Torque when a job is submitted and the requested
    slots per node are bigger that the number of available vcpus on a single
    instance the jobwatcher is selecting a bigger number of nodes to fit the
    requested CPUs. This operation/assumption is incorrect since the scheduler
    will never schdule the job due to the fact that the CPU contraints are never
    satisfied. This causes the cluster to spin up new capacity that will stay
    idle forever or until the job is canceled.
    This commit fixes the above mentioned issue.
    
    Signed-off-by: Francesco De Martino <fdm@amazon.com>

--- jobwatcher/plugins/utils.py
+++ jobwatcher/plugins/utils.py
@@ -15,18 +15,18 @@ def get_optimal_nodes(nodes_requested, slots_requested, instance_properties):
     vcpus = instance_properties.get("slots")
     slots_remaining_per_node = []
 
-    for node_idx, num_of_nodes in enumerate(nodes_requested):
-        log.info("Requested %s nodes and %s slots" % (num_of_nodes, slots_requested[node_idx]))
+    for slots, num_of_nodes in zip(slots_requested, nodes_requested):
+        log.info("Requested %s nodes and %s slots" % (num_of_nodes, slots))
         # For simplicity, uniformly distribute the numbers of cpus requested across all the requested nodes
-        slots_required_per_node = -(-slots_requested[node_idx] // num_of_nodes)
+        slots_required_per_node = -(-slots // num_of_nodes)
 
         if slots_required_per_node > vcpus:
-            # If slots required per node is greater than vcpus, add additional nodes
-            # and recalculate slots_required_per_node
-            log.info("Slots required per node is greater than vcpus, recalculating")
-            num_of_nodes = -(-slots_requested[node_idx] // vcpus)
-            slots_required_per_node = -(-slots_requested[node_idx] // num_of_nodes)
-            log.info("Recalculated: %s nodes and %s slots required per node" % (num_of_nodes, slots_required_per_node))
+            log.warning(
+                "Slots required per node (%d) is greater than vcpus available on single node (%d), skipping job...",
+                slots_required_per_node,
+                vcpus,
+            )
+            continue
 
         # Verify if there are enough available slots in the nodes allocated in the previous rounds
         for slot_idx, slots_available in enumerate(slots_remaining_per_node):
