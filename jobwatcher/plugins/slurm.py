a071feceaba388e887ae2e53aeaed488780307bc
Francesco De Martino <fdm@amazon.com>
2019-05-01 14:58:02 +0200

    jobwatcher - slurm: add down nodes to busy nodes count
    This is done to speedup cluster recovery in case of failing
    nodes. jobwatcher will start requesting an additional node if
    possible while nodewatcher will take care of terminating the
    faulty instance.
    
    Signed-off-by: Francesco De Martino <fdm@amazon.com>

--- jobwatcher/plugins/slurm.py
+++ jobwatcher/plugins/slurm.py
@@ -34,16 +34,17 @@ def get_required_nodes(instance_properties):
 
 # get nodes reserved by running jobs
 def get_busy_nodes(instance_properties):
-    command = "/opt/slurm/bin/sinfo -r -h -o '%D %t'"
+    command = "/opt/slurm/bin/sinfo -h -o '%D %t'"
     # Sample output:
     # 2 mix
     # 4 alloc
     # 10 idle
+    # 1 down*
     output = check_command_output(command)
     nodes = 0
     output = output.split("\n")
     for line in output:
         line_arr = line.split()
-        if len(line_arr) == 2 and (line_arr[1] in ["mix", "alloc", "drain", "drain*"]):
+        if len(line_arr) == 2 and (line_arr[1] in ["mix", "alloc", "drain", "drain*", "down", "down*"]):
             nodes += int(line_arr[0])
     return nodes
