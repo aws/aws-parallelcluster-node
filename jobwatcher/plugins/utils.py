8838a6f26ccc61ec510821d2d4cd98d0fbf00200
Francesco De Martino <fdm@amazon.com>
2019-05-27 22:43:40 +0200

    Revert a fix in get_optimal_nodes to allow additional testing with torque
    Note: sge and slurm are not affected by this
    Signed-off-by: Francesco De Martino <fdm@amazon.com>

--- jobwatcher/plugins/utils.py
+++ jobwatcher/plugins/utils.py
@@ -21,12 +21,19 @@ def get_optimal_nodes(nodes_requested, slots_requested, instance_properties):
         slots_required_per_node = -(-slots // num_of_nodes)
 
         if slots_required_per_node > vcpus:
-            log.warning(
-                "Slots required per node (%d) is greater than vcpus available on single node (%d), skipping job...",
-                slots_required_per_node,
-                vcpus,
-            )
-            continue
+            # FIXME: Replace this code with the commented section below after testing with Torque
+            # If slots required per node is greater than vcpus, add additional nodes
+            # and recalculate slots_required_per_node
+            log.info("Slots required per node is greater than vcpus, recalculating")
+            num_of_nodes = -(-slots // vcpus)
+            slots_required_per_node = -(-slots // num_of_nodes)
+            log.info("Recalculated: %s nodes and %s slots required per node" % (num_of_nodes, slots_required_per_node))
+            # log.warning(
+            #     "Slots required per node (%d) is greater than vcpus available on single node (%d), skipping job...",
+            #     slots_required_per_node,
+            #     vcpus,
+            # )
+            # continue
 
         # Verify if there are enough available slots in the nodes allocated in the previous rounds
         for slot_idx, slots_available in enumerate(slots_remaining_per_node):
