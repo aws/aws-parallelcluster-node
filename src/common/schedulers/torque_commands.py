3134df4d51b6a699c433df7569592d137b4ecbf3
Francesco De Martino <fdm@amazon.com>
2019-07-01 16:17:18 +0200

    sqswatcher - torque: refactoring + performance enhancements
    - Handle nodes add/removal in parallel
    - Trigger scheduling cycle only when the first node gets added to cluster
    - Add logic to handle retries on failures
    
    Signed-off-by: Francesco De Martino <fdm@amazon.com>

---
+++ src/common/schedulers/torque_commands.py
@@ -0,0 +1,159 @@
+# Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
+# with the License. A copy of the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "LICENSE.txt" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+# OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
+# limitations under the License.
+
+# In Python 2, division of two ints produces an int. In Python 3, it produces a float. Uniforming the behaviour.
+import logging
+import re
+import subprocess
+from xml.etree import ElementTree
+
+from retrying import retry
+
+from common.schedulers.converters import ComparableObject, from_xml_to_obj
+from common.time_utils import minutes, seconds
+from common.utils import check_command_output, run_command
+
+ERROR_NODE_STATES = ("down", "offline", "unknown")
+NODE_STATES = (
+    "free",
+    "offline",
+    "down",
+    "reserve",
+    "job-exclusive",
+    "job-sharing",
+    "busy",
+    "time-shared",
+    "state-unknown",
+)
+
+
+def _qmgr_manage_nodes(operation, hosts, error_messages_to_ignore, additional_qmgr_args=""):
+    if not hosts:
+        return []
+
+    hostnames = ",".join(hosts)
+    command = '/opt/torque/bin/qmgr -c "{operation} node {hostnames} {additional_args}"'.format(
+        operation=operation, hostnames=hostnames, additional_args=additional_qmgr_args
+    )
+    try:
+        output = check_command_output(command, log_error=False)
+    except subprocess.CalledProcessError as e:
+        if not hasattr(e, "output") or e.output == "":
+            logging.error("Failed when executing operation %s on nodes %s with error %s", operation, hostnames, e)
+            return set()
+
+    succeeded_hosts = set(hosts)
+    # analyze command output to understand if failure can be ignored (e.g. already existing node)
+    for error_message in output.splitlines():
+        match = re.match(r"qmgr obj=(?P<host>.*) svr=default: (?P<error>.*)", error_message)
+        if not match:
+            # assume unexpected error and mark all as failed
+            logging.error("Failed when executing operation %s on nodes %s with error %s", operation, hostnames, output)
+            return set()
+
+        host, error = match.groups()
+        if any(error == message_to_ignore for message_to_ignore in error_messages_to_ignore):
+            logging.warning(
+                "Marking %s operation on node % as succeeded because of ignored error message %s",
+                operation,
+                host,
+                error,
+            )
+            continue
+
+        try:
+            succeeded_hosts.remove(host)
+        except Exception as e:
+            logging.error(
+                "Failed to extract host from error message while adding nodes. Mark all as failed. Output was %s.\n"
+                "Exception was %s",
+                output,
+                e,
+            )
+
+    return succeeded_hosts
+
+
+def add_nodes(hosts, slots):
+    return _qmgr_manage_nodes(
+        operation="create",
+        hosts=hosts,
+        error_messages_to_ignore=["Node name already exists"],
+        additional_qmgr_args="np={0}".format(slots),
+    )
+
+
+def delete_nodes(hosts):
+    return _qmgr_manage_nodes(operation="delete", hosts=hosts, error_messages_to_ignore=["Unknown node"])
+
+
+def get_compute_nodes_info(hostname_filter=None):
+    command = "/opt/torque/bin/pbsnodes -x"
+    if hostname_filter:
+        command += " {0}".format(" ".join(hostname_filter))
+
+    output = check_command_output(command)
+    root = ElementTree.fromstring(output)
+    nodes = root.findall("./Node")
+    nodes_list = [TorqueHost.from_xml(ElementTree.tostring(node)) for node in nodes]
+    return dict((node.name, node) for node in nodes_list)
+
+
+@retry(wait_fixed=seconds(3), retry_on_result=lambda result: result is False, stop_max_delay=minutes(1))
+def wait_nodes_initialization(hosts):
+    """Wait for at least one host from hosts to become active"""
+    torque_hosts = get_compute_nodes_info(hosts).values()
+    for node in torque_hosts:
+        if not any(init_state in node.state for init_state in ERROR_NODE_STATES):
+            return True
+    return False
+
+
+def wakeup_scheduler(added_hosts):
+    torque_hosts = get_compute_nodes_info().values()
+    for node in torque_hosts:
+        if not any(init_state in node.state for init_state in ERROR_NODE_STATES):
+            if node.name not in added_hosts:
+                # Do not trigger scheduling cycle when there was already at least one active node.
+                return
+
+    try:
+        # Before triggering a scheduling cycle wait for at least one node to become active
+        wait_nodes_initialization(added_hosts)
+    except Exception as e:
+        logging.error("Failed while waiting for nodes initialization with exception %s", e)
+
+    # Trigger a scheduling cycle. This is necessary when the first compute node gets added to the scheduler.
+    logging.info("Triggering a scheduling cycle.")
+    run_command('/opt/torque/bin/qmgr -c "set server scheduling=true"', raise_on_error=False)
+
+
+class TorqueHost(ComparableObject):
+    # <Node>
+    #     <name>ip-10-0-1-242</name>
+    #     <state>free</state>
+    #     <power_state>Running</power_state>
+    #     <np>4</np>
+    #     <ntype>cluster</ntype>
+    #     <status>opsys=linux,uname=Linux ip-10-0-1-242 4.4.111-1.el6.elrepo.x86_64...</status>
+    #     <mom_service_port>15002</mom_service_port>
+    #     <mom_manager_port>15003</mom_manager_port>
+    # </Node>
+    MAPPINGS = {"name": {"field": "name"}, "np": {"field": "slots", "transformation": int}, "state": {"field": "state"}}
+
+    def __init__(self, name=None, slots=0, state=""):
+        self.name = name
+        self.slots = slots
+        self.state = state
+
+    @staticmethod
+    def from_xml(xml):
+        return from_xml_to_obj(xml, TorqueHost)
