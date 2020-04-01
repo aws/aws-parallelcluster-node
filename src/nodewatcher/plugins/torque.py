f22b8448e41fd55b4e976a799f35ff55624de24c
Tim Lane <tilne@amazon.com>
2020-03-31 18:31:02 -0700

    Standardize logging when getting pending jobs
    Generate log messages describing the pending jobs found in the common
    scheduler commands modules instead of duplicating the same calls in the
    scheduler-specific plugins for each of the daemons.
    
    Signed-off-by: Tim Lane <tilne@amazon.com>

--- src/nodewatcher/plugins/torque.py
+++ src/nodewatcher/plugins/torque.py
@@ -49,7 +49,6 @@ def has_pending_jobs(instance_properties, max_size):
     """
     try:
         pending_jobs = get_pending_jobs_info(max_slots_filter=instance_properties.get("slots"))
-        logging.info("Found the following pending jobs:\n%s", pending_jobs)
         return len(pending_jobs) > 0, False
     except Exception as e:
         log.error("Failed when checking for pending jobs with exception %s. Reporting no pending jobs.", e)
