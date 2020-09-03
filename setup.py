f30caa202272a32002ee5350f4d1f9b9fe6f4c35
Francesco De Martino <fdm@amazon.com>
2020-09-03 20:05:49 +0200

    Bump version to 2.9.0
    Signed-off-by: Francesco De Martino <fdm@amazon.com>

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ console_scripts = [
     "clustermgtd = slurm_plugin.clustermgtd:main",
     "computemgtd = slurm_plugin.computemgtd:main",
 ]
-version = "2.8.1"
+version = "2.9.0"
 requires = ["boto3>=1.7.55", "retrying>=1.3.3", "paramiko>=2.4.2"]
 
 setup(
