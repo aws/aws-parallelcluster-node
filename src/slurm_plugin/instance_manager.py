a2482a4227a18da9e05e256868d789a7184a4b5f
chenwany <chenwany@amazon.com>
2021-06-30 15:13:13 -0700

    3.0 Apply Rename tag after applying protected_mode
    Apply rename tag ClusterName to parallelcluster:cluster-name
    Apply change tag prefix to 'parallelcluster:'
    
    Signed-off-by: chenwany <chenwany@amazon.com>

--- src/slurm_plugin/instance_manager.py
+++ src/slurm_plugin/instance_manager.py
@@ -320,12 +320,12 @@ class InstanceManager:
         ec2_client = boto3.client("ec2", region_name=self._region, config=self._boto3_config)
         paginator = ec2_client.get_paginator("describe_instances")
         args = {
-            "Filters": [{"Name": "tag:ClusterName", "Values": [self._cluster_name]}],
+            "Filters": [{"Name": "tag:parallelcluster:cluster-name", "Values": [self._cluster_name]}],
         }
         if alive_states_only:
             args["Filters"].append({"Name": "instance-state-name", "Values": list(EC2_INSTANCE_ALIVE_STATES)})
         if not include_head_node:
-            args["Filters"].append({"Name": "tag:aws-parallelcluster-node-type", "Values": ["Compute"]})
+            args["Filters"].append({"Name": "tag:parallelcluster:node-type", "Values": ["Compute"]})
         response_iterator = paginator.paginate(PaginationConfig={"PageSize": BOTO3_PAGINATION_PAGE_SIZE}, **args)
         filtered_iterator = response_iterator.search("Reservations[].Instances[]")
         return [
