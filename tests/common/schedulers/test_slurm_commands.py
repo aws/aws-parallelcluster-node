eb03cc62a9a17a617784b8ea3644e41856136d0e
Nse Obot <nseobot@amazon.com>
2022-09-26 15:22:26 -0400

    do tox autoformat

--- tests/common/schedulers/test_slurm_commands.py
+++ tests/common/schedulers/test_slurm_commands.py
@@ -586,77 +586,75 @@ def test_slurm_node_is_rebooting(node, expected_output):
         (SlurmNode("queue1-st-c5xlarge-1", "nodeip", "nodehostname", "somestate", "queue1"), False),
         (SlurmNode("queue1-st-c5xlarge-1", "nodeip", "nodehostname", "MIXED+CLOUD+DOWN+POWERING_UP", "queue1"), True),
         (
-                SlurmNode(
-                    "queue1-st-c5xlarge-1", "nodeip", "nodehostname", "ALLOCATED+CLOUD+DRAIN+NOT_RESPONDING", "queue1"
-                ),
-                False,
+            SlurmNode(
+                "queue1-st-c5xlarge-1", "nodeip", "nodehostname", "ALLOCATED+CLOUD+DRAIN+NOT_RESPONDING", "queue1"
+            ),
+            False,
         ),
         (SlurmNode("queue1-dy-c5xlarge-1", "nodeip", "nodehostname", "DOWN+CLOUD+NOT_RESPONDING", "queue1"), True),
         (SlurmNode("queue1-dy-c5xlarge-1", "nodeip", "nodehostname", "DOWN+CLOUD+POWER", "queue1"), True),
         (SlurmNode("queue1-dy-c5xlarge-1", "nodeip", "nodehostname", "IDLE+CLOUD+POWERING_DOWN", "queue1"), False),
         (
-                SlurmNode(
-                    "queue1-dy-c5xlarge-1",
-                    "nodeip",
-                    "nodehostname",
-                    "DOWN+CLOUD+POWER_DOWN+POWERED_DOWN",
-                    "queue1",
-                ),
-                True,
+            SlurmNode(
+                "queue1-dy-c5xlarge-1",
+                "nodeip",
+                "nodehostname",
+                "DOWN+CLOUD+POWER_DOWN+POWERED_DOWN",
+                "queue1",
+            ),
+            True,
         ),
         (
-                SlurmNode(
-                    "queue1-dy-c5xlarge-1",
-                    "nodeip",
-                    "nodehostname",
-                    "DOWN+CLOUD+POWER_DOWN",
-                    "queue1",
-                ),
-                False,
+            SlurmNode(
+                "queue1-dy-c5xlarge-1",
+                "nodeip",
+                "nodehostname",
+                "DOWN+CLOUD+POWER_DOWN",
+                "queue1",
+            ),
+            False,
         ),
         (
-                SlurmNode(
-                    "queue1-dy-c5xlarge-1",
-                    "nodeip",
-                    "nodehostname",
-                    "DOWN+CLOUD+POWERING_DOWN",
-                    "queue1",
-                ),
-                False,
+            SlurmNode(
+                "queue1-dy-c5xlarge-1",
+                "nodeip",
+                "nodehostname",
+                "DOWN+CLOUD+POWERING_DOWN",
+                "queue1",
+            ),
+            False,
         ),
         (
-                SlurmNode(
-                    "queue1-dy-c5xlarge-1",
-                    "nodeip",
-                    "nodehostname",
-                    "DOWN+CLOUD+POWERING_DOWN+POWERED_DOWN",
-                    "queue1",
-                ),
-                False,
+            SlurmNode(
+                "queue1-dy-c5xlarge-1",
+                "nodeip",
+                "nodehostname",
+                "DOWN+CLOUD+POWERING_DOWN+POWERED_DOWN",
+                "queue1",
+            ),
+            False,
         ),
         (
-                SlurmNode(
-                    "queue1-dy-c5xlarge-1",
-                    "nodeip",
-                    "nodehostname",
-                    "DOWN+CLOUD+POWER_DOWN+POWERING_DOWN+POWERED_DOWN",
-                    "queue1",
-                ),
-                False,
+            SlurmNode(
+                "queue1-dy-c5xlarge-1",
+                "nodeip",
+                "nodehostname",
+                "DOWN+CLOUD+POWER_DOWN+POWERING_DOWN+POWERED_DOWN",
+                "queue1",
+            ),
+            False,
         ),
         (
-                SlurmNode("queue1-dy-c5xlarge-1", "nodeip", "nodehostname", "DOWN+CLOUD+REBOOT_ISSUED", "queue1"),
-                True,
+            SlurmNode("queue1-dy-c5xlarge-1", "nodeip", "nodehostname", "DOWN+CLOUD+REBOOT_ISSUED", "queue1"),
+            True,
         ),
         (
-                SlurmNode(
-                    "queue1-st-c5xlarge-1", "nodeip", "nodehostname", "MIXED+CLOUD+DRAIN+REBOOT_REQUESTED", "queue1"
-                ),
-                False,
+            SlurmNode("queue1-st-c5xlarge-1", "nodeip", "nodehostname", "MIXED+CLOUD+DRAIN+REBOOT_REQUESTED", "queue1"),
+            False,
         ),
         (
-                SlurmNode("queue1-st-c5xlarge-1", "nodeip", "nodehostname", "DOWN+CLOUD+DRAIN+REBOOT_ISSUED", "queue1"),
-                True,
+            SlurmNode("queue1-st-c5xlarge-1", "nodeip", "nodehostname", "DOWN+CLOUD+DRAIN+REBOOT_ISSUED", "queue1"),
+            True,
         ),
     ],
 )
