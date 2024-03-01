94064fddcf4633bf125cc6e3068cafc747e3f56c
Himani Anil Deshpande <79726937+himani2411@users.noreply.github.com>
2024-03-01 11:13:50 -0500

    Bump version to 3.10.0 (#623)
    Signed-off-by: Himani Deshpande <himanidp@amazon.com>
    Co-authored-by: Himani Deshpande <himanidp@amazon.com>

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ console_scripts = [
     "clustermgtd = slurm_plugin.clustermgtd:main",
     "computemgtd = slurm_plugin.computemgtd:main",
 ]
-version = "3.9.0"
+version = "3.10.0"
 requires = ["boto3>=1.7.55", "retrying>=1.3.3"]
 
 setup(
